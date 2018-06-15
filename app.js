7f84237bd6df2828a4eb60e8ad4e3b3a821cf7de
Varunkumar Karunakaran <vkarunakaran@polarismanagement.com>
2018-06-15 15:52:01 -0400

    app change

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var app = require('./server.js');
 
 
-app.get('/gapi/notifcations', (req, res) => {
+app.get('/gapi', (req, res) => {
   res.send('GET');
 });
 
-app.post('/gapi/notifcations', (req, res) => {
+app.post('/gapi/notifications', (req, res) => {
   console.log(req);
   console.log(res);
   res.send('POST');
